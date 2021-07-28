7d3e60b1e3dc56e5cc6a3561c4042b1c4f979094
Franklin José Vasquez Villar - Ceiba Software <franklin.vasquez@ceiba.com.co>
2021-07-28 17:07:07 -0500

    eliminar css vacios

--- src/app/feature/cita/components/cita/cita.component.ts
+++ src/app/feature/cita/components/cita/cita.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cita',
   templateUrl: './cita.component.html',
-  styleUrls: ['./cita.component.css']
 })
 export class CitaComponent implements OnInit {
 
